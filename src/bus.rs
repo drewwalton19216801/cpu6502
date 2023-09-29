6589e6570fef90d0e41a64161b076385af4a9527
Drew Walton <drewwalton19216801@gmail.com>
2023-09-29 14:10:39 -0600

    Improve documentation

--- src/bus.rs
+++ src/bus.rs
@@ -1,14 +1,39 @@
+//! # Bus
+//! 
+//! The bus is the component that connects all the devices together. It is responsible for
+//! reading and writing to the devices. It also loads the ROM into memory.
 use crate::device::Device;
 
+/// Trait representing a bus that can be read from and written to. Generally you will want to
+/// only use a single bus for your emulator.
 pub trait Bus {
+    /// Adds a device to the bus.
+    ///
+    /// # Arguments
+    ///
+    /// * `device` - A boxed trait object that implements the `Device` trait.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use cpu6502::bus::Bus;
+    /// # use cpu6502::device::Device;
+    /// # struct TestDevice;
+    /// # impl Device for TestDevice {}
+    /// let mut bus = Bus::new();
+    /// let device = Box::new(TestDevice {});
+    /// bus.add_device(device);
+    /// ```
     fn add_device(&mut self, device: Box<dyn Device>);
     fn read(&mut self, address: u16) -> u8;
     fn write(&mut self, address: u16, value: u8);
     fn load_rom_at(&mut self, rom: &[u8], address: u16);
 }
 
+/// A default implementation of a bus.
 pub struct DefaultBus;
 
+/// Implements the Bus trait for the DefaultBus struct.
 impl Bus for DefaultBus {
     fn add_device(&mut self, _device: Box<dyn Device>) {
         println!("Adding device");
@@ -28,6 +53,7 @@ impl Bus for DefaultBus {
     }
 }
 
+/// Implements the default behavior for the `DefaultBus` struct.
 impl Default for DefaultBus {
     fn default() -> Self {
         Self {}
